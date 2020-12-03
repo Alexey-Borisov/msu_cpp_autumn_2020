142f5a81c06b4098f0209b922d676bc8c0bafb10
Alexey-Borisov <72136589+Alexey-Borisov@users.noreply.github.com>
2020-12-03 17:51:54 +0300

    Add files via upload

--- 08/test.cpp
+++ 08/test.cpp
@@ -19,6 +19,7 @@ int32_t is_prime(int32_t num){
 }
 
 //Поиск n-ого простого числа
+//Использовал чтобы потоки работали не очень маленькое время.
 int32_t n_prime(int32_t n){
     int32_t prime_num = 0;
     int32_t cur_prime = 0, cur_num = 2;
@@ -34,43 +35,43 @@ int32_t n_prime(int32_t n){
 
 void DefaultWorkTest(){
     ThreadPool pool(3);
-    auto thread_1 = pool.exec(n_prime, 30);
-    auto thread_2 = pool.exec(n_prime, 20);
-    auto thread_3 = pool.exec(n_prime, 10);
+    auto thread_1 = pool.exec(n_prime, 30000);
+    auto thread_2 = pool.exec(n_prime, 20000);
+    auto thread_3 = pool.exec(n_prime, 10000);
     auto res_1 = thread_1.get();
     auto res_2 = thread_2.get();
     auto res_3 = thread_3.get();
-    assert(res_1 == n_prime(30) &&
-            res_2 == n_prime(20) &&
-            res_3 == n_prime(10));
+    assert(res_1 == n_prime(30000) &&
+            res_2 == n_prime(20000) &&
+            res_3 == n_prime(10000));
 }
 
 void LotsOfTasksTest(){
     ThreadPool pool(2);
-    auto thread_1 = pool.exec(n_prime, 60);
-    auto thread_2 = pool.exec(n_prime, 50);
-    auto thread_3 = pool.exec(n_prime, 40);
-    auto thread_4 = pool.exec(n_prime, 30);
-    auto thread_5 = pool.exec(n_prime, 20);
-    auto thread_6 = pool.exec(n_prime, 10);
+    auto thread_1 = pool.exec(n_prime, 60000);
+    auto thread_2 = pool.exec(n_prime, 50000);
+    auto thread_3 = pool.exec(n_prime, 40000);
+    auto thread_4 = pool.exec(n_prime, 30000);
+    auto thread_5 = pool.exec(n_prime, 20000);
+    auto thread_6 = pool.exec(n_prime, 10000);
     auto res_1 = thread_1.get();
     auto res_2 = thread_2.get();
     auto res_3 = thread_3.get();
     auto res_4 = thread_4.get();
     auto res_5 = thread_5.get();
     auto res_6 = thread_6.get();
-    assert(res_1 == n_prime(60) &&
-            res_2 == n_prime(50) &&
-            res_3 == n_prime(40) &&
-            res_4 == n_prime(30) &&
-            res_5 == n_prime(20) &&
-            res_6 == n_prime(10));
+    assert(res_1 == n_prime(60000) &&
+            res_2 == n_prime(50000) &&
+            res_3 == n_prime(40000) &&
+            res_4 == n_prime(30000) &&
+            res_5 == n_prime(20000) &&
+            res_6 == n_prime(10000));
 }
 
 void WithoutGetTest(){
-    ThreadPool pool(18);
-    for(size_t i = 0; i < 18; ++i){
-        auto thread = pool.exec([]{return 0; });
+    ThreadPool pool(100);
+    for(size_t i = 0; i < 100; ++i){
+        auto thread = pool.exec([i]{return n_prime((i + 1) * 100); });
     }
 }
 
